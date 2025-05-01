5864351388b39b5de2ade04199ef49e7cd564bb7
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2025-05-01 10:23:41 -0600

    Update SIMON.py

--- SIMON.py
+++ SIMON.py
@@ -7,6 +7,35 @@ pygame.init()
 pygame.display.set_caption("!")
 screen = pygame.display.set_mode((800,800))
 
+def collision(xpos, ypos):
+    if math.sqrt((xpos - 400)**2 + (ypos - 400)**2) > 200:
+        #print("OUT")
+        return -1
+    elif xpos < 400 and ypos < 400:
+        #print("GREEN")
+        pygame.draw.arc(screen, (0, 255, 0), (200, 200, 400, 400), pi / 2, pi, 100)
+        pygame.display.flip()
+        winsound.Beep(640, 500)
+        return 0
+    elif xpos < 400 and ypos > 400:
+        #print("YELLOW")
+        pygame.draw.arc(screen, (255, 255, 0), (200, 200, 400, 400), pi, (3 * pi / 2), 100)
+        pygame.display.flip()
+        winsound.Beep(240, 500)
+        return 1
+    elif xpos > 400 and ypos > 400:
+        #print("RED")
+        pygame.draw.arc(screen, (255, 0, 0), (200, 200, 400, 400), 0, pi / 2, 100) 
+        pygame.display.flip()
+        winsound.Beep(440, 500)
+        return 2
+    elif xpos > 400 and ypos < 400:
+        #print("BLUE")
+        pygame.draw.arc(screen, (0, 0, 255), (200, 200, 400, 400), (3 * pi / 2), 2 * pi, 100) 
+        pygame.display.flip()
+        winsound.Beep(40, 500)
+        return 3
+
 # game variables
 xpos = 0
 ypos = 0
@@ -21,12 +50,6 @@ pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062
 7245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094
 3305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912
 ded = False
-
-def collision(xpos, ypos):
-    if math.sqrt((xpos - 400)**2 + (ypos - 400)**2) > 200:
-        print("OUT")
-    else:
-        print("IN")
 
 # game loop ###############################################################################################
 while True:
@@ -80,6 +103,9 @@ while True:
     pygame.draw.arc(screen, (0, 0, 155), (200, 200, 400, 400), (3 * pi / 2), 2 * pi, 100)
     pygame.display.flip()
     #pygame.time.wait(800)
+
+    playerPattern.append(collision(mousePos[0], mousePos[1]))
+    print(playerPattern)
 
     # render
     pygame.draw.arc(screen, (0, 155, 0), (200, 200, 400, 400), pi / 2, pi, 100) # GREEN
